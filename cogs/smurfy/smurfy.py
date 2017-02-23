583e0d5a590edb900211553b2d9d64aa69859d22
TsundereStrike <aestheticartist@gmail.com>
2017-02-22 23:28:54 -0500

    get json from request

--- cogs/smurfy/smurfy.py
+++ cogs/smurfy/smurfy.py
@@ -24,44 +24,50 @@ class Smurfy:
     @commands.command()
     async def pricelist(self):
         """Request price list of almost all in-game objects (mechs, items..)"""
-        url1 = "http:/mwo.smurfy-net.de/api/data/prices.json"
+        url1 = "http://mwo.smurfy-net.de/api/data/prices.json"
         list1 = requests.get(url=url1)
-        await self.bot.say(list1)
+        json1 = list1.json
+        await self.bot.say(json1)
 
     @commands.command()
     async def modulelist(self):
         """Request list of modules"""
         url2 = "http://mwo.smurfy-net.de/api/data/modules.json"
         list2 = requests.get(url=url2)
-        await self.bot.say(list2)
+        json2 = list2.json
+        await self.bot.say(json2)
 
     @commands.command()
     async def weaponlist(self):
         """Request list of weapons"""
         url3 = "http://mwo.smurfy-net.de/api/data/weapons.json"
         list3 = requests.get(url=url3)
-        await self.bot.say(list3)
+        json3 = list3.json
+        await self.bot.say(json3)
 
     @commands.command()
     async def ammolist(self):
         """Request list of ammo"""
         url4 = "http://mwo.smurfy-net.de/api/data/ammo.json"
         list4 = requests.get(url=url4)
-        await self.bot.say(list4)
+        json4 = list4.json
+        await self.bot.say(json4)
 
     @commands.command()
     async def omnipodlist(self):
         """Request list of omnipods"""
         url5 = "http://mwo.smurfy-net.de/api/data/omnipods.json"
         list5 = requests.get(url=url5)
-        await self.bot.say(list5)
+        json5 = list5.json
+        await self.bot.say(json5)
 
     @commands.command()
     async def mechlist(self):
         """Request list of mechs"""
         url6 = "http://mwo.smurfy-net.de/api/data/mechs.json"
         list6 = requests.get(url=url6)
-        await self.bot.say(list6)
+        json6 = list6.json
+        await self.bot.say(json6)
 
     @commands.command()
     async def mechbyid(self):
@@ -69,7 +75,8 @@ class Smurfy:
         # todo - 1 needs to be user input id
         url7 = "http://mwo.smurfy-net.de/api/data/mechs/1.json"
         list7 = requests.get(url=url7)
-        await self.bot.say(list7)
+        json7 = list7.json
+        await self.bot.say(json7)
 
     @commands.command()
     async def mechloadout(self):
@@ -80,7 +87,8 @@ class Smurfy:
         # 75a1ea4fcf79e1c24216b1db218a47e6f9888309 needs to be user input loadout id
         url8 = "http://mwo.smurfy-net.de/api/data/mechs/415/loadouts/75a1ea4fcf79e1c24216b1db218a47e6f9888309.json?print=pretty"
         list8 = json.load(urllib2.urlopen(url8))
-        await self.bot.say(list8)
+        json8 = list8.json
+        await self.bot.say(json8)
             
 def setup(bot):
     bot.add_cog(Smurfy(bot))
